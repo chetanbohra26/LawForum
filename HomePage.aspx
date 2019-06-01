aad6725a82fbec04e64e4f12b3422d707d63d9ca
chetanbohra26 <chetanbohra26@gmail.com>
2019-06-01 16:42:31 +0530

    Initial commit

---
+++ HomePage.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="HomePage.aspx.cs" Inherits="HomePage" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="position:relative;top:0px">
+        <asp:Label ID="LblHomeMsgUserName" runat="server" Text="Welcome User" ForeColor="Green" Width="100%" style="text-align:center" /><br />
+    </div>
+    <div style="position:absolute;top:10px;right:10px">
+        <asp:LinkButton ID="BtnLogout" runat="server" Text="Logout" onclick="BtnLogout_Click" />
+    </div>
+    <br />
+    <div style="position:relative;top:10" >
+        <asp:Label ID="LblPost1" runat="server" Text="Post your query for experts to answer..." /><br />
+        <asp:TextBox ID="TxtPostTitle" runat="server" placeholder="Enter Post Title"/><br />
+        <asp:TextBox ID="TxtPostDescription" runat="server" placeholder="Enter Post Description"
+            TextMode="multiline" Columns="50" Rows="5" /><br />
+        <asp:Button ID="BtnSubmitPost" runat="server" Text="Submit Post" 
+            onclick="BtnSubmitPost_Click" /><br />
+        <asp:Label ID="LblPostStatus" runat="server" Visible="false" Font-Bold="true" ForeColor="Red" /><br />
+        <hr />
+        <asp:Label ID="LblHomeMessage" runat="server" Text="Or select one of the queries below to view content...." /><br />
+        <asp:DataList ID="ListPosts" runat="server" >
+            <ItemTemplate>
+                <table cellpadding="10" cellspacing="10" border="1" width="500px" style="border:dashed 2px">
+                    <tr>
+                        <tr >  
+                        <td>  
+                            <b>ID: </b><asp:LinkButton ID="PostLink" runat="server" Text='<%# Eval("post_id") %>' onclick="PostLink_Click" /><br />  
+                            <b>Title: </b><%# Eval("post_title") %><br />  
+                            <b>Posted by: </b><%# Eval("user_name")%><br />  
+                        </td>  
+                    </tr>
+                </table>
+            </ItemTemplate>
+        </asp:DataList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ HomePage.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class HomePage : System.Web.UI.Page
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    SqlDataReader dr;
+    private UInt64 user_id = 0;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String name = getUname();
+        if (name == null)
+        {
+            Response.Redirect("WelcomePage.aspx");
+        }
+        else
+        {
+            LblHomeMsgUserName.Text = "Welcome " + name;
+        }
+
+        loadPosts();
+    }
+
+    public String getUname()
+    {
+        try
+        {
+            user_id = UInt64.Parse(Session["user_id"].ToString());
+            con = SqlHelper.getInstance();
+            con.Open();
+            cmd = new SqlCommand("select user_name from Users where user_id=" + user_id, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            return dr[0].ToString();
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    protected void BtnLogout_Click(object sender, EventArgs e)
+    {
+        Session["user_id"] = 0;
+        Response.Redirect("WelcomePage.aspx");
+    }
+
+    private void loadPosts()
+    {
+        con = SqlHelper.getInstance();
+        con.Open();
+        cmd = new SqlCommand("select post_id,post_title,user_name from Posts,Users where Posts.user_id=Users.user_id", con);
+        ListPosts.DataSource = cmd.ExecuteReader();
+        ListPosts.DataBind();
+    }
+
+    protected void PostLink_Click(object sender, EventArgs e)
+    {
+        dr.Close();
+        con.Close();
+        Response.Redirect("PostPage.aspx?post_id=" + ((LinkButton)sender).Text);
+    }
+    protected void BtnSubmitPost_Click(object sender, EventArgs e)
+    {
+        String title = TxtPostTitle.Text.ToString().Trim();
+        String desc = TxtPostDescription.Text.ToString().Trim();
+
+        if (title.Length == 0)
+        {
+            LblPostStatus.Text = "Title cannot be empty";
+            LblPostStatus.ForeColor = System.Drawing.Color.Red;
+            LblPostStatus.Visible = true;
+            return;
+        }
+        if (desc.Length == 0)
+        {
+            LblPostStatus.Text = "Decription cannot be empty";
+            LblPostStatus.ForeColor = System.Drawing.Color.Red;
+            LblPostStatus.Visible = true;
+            return;
+        }
+        con = SqlHelper.getInstance();
+        con.Open();
+        cmd = new SqlCommand("insert into Posts(user_id,post_title,post_desc) values(" + user_id + ",'" + title + "','" + desc + "')", con);
+        int flag = cmd.ExecuteNonQuery();
+        if (flag != 1)
+        {
+            LblPostStatus.Text = "Could not submit post";
+            LblPostStatus.ForeColor = System.Drawing.Color.Red;
+            LblPostStatus.Visible = true;
+        }
+        else
+        {
+            LblPostStatus.Text = "Success";
+            LblPostStatus.ForeColor = System.Drawing.Color.Green;
+            LblPostStatus.Visible = true;
+            TxtPostTitle.Text = "";
+            TxtPostDescription.Text = "";
+        }
+    }
+}
